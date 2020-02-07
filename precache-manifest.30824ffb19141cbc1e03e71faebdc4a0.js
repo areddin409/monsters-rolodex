13f3c20132b01df1a613fc48722418142378e180
Andrew <areddin409@gmail.com>
2020-02-06 18:06:53 -0600

    Updates

---
+++ precache-manifest.30824ffb19141cbc1e03e71faebdc4a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5e30d34e95f822d95d73f493391a1e5",
+    "url": "/monsters-rolodex-2/index.html"
+  },
+  {
+    "revision": "a0b91683e91fb7109c15",
+    "url": "/monsters-rolodex-2/static/css/main.c7b462f9.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex-2/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex-2/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0b91683e91fb7109c15",
+    "url": "/monsters-rolodex-2/static/js/main.5849fa14.chunk.js"
+  },
+  {
+    "revision": "752eb8735824cf913194",
+    "url": "/monsters-rolodex-2/static/js/runtime-main.3b6e64e6.js"
+  }
+]);
